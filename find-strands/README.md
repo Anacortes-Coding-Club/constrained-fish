03bd4a2c42855c3fd37ec291afa4cb33f8b48d23
Nat Peterson <npeterson@gmail.com>
2024-03-31 14:09:12 -0700

    strands solver start

---
+++ find-strands/README.md
@@ -0,0 +1,14 @@
+# Stands Solver
+
+Here is a first go at a solver for the NYT game [Strands](https://www.nytimes.com/games/strands)
+Rather than running on the web, this uses [node.js](https://nodejs.org/en) to load the dictionary. 
+
+It's just a start, because it finds too many words! From a given start point (0-indexed) rows and columns, the `explore` function finds many words. For the sample puzzle, starting from the bottom row, column 4, indeed finds the spangram "illumination" ... and 32 other words!
+
+So where to go from here? We could teach it a few more rules. For example, it appears that spangrams always start and end on the edge, but the instructions say it only needs touch opposite sides of the board.
+
+A more promising rule is that "no theme words overlap" which sounds like no "X" crossing? Though it may just mean no re-using letters. The 'no "X" crossing' rule is a little tricky to code, but certainly possible. It would still find many words, and may not be correct.
+
+Further work could continue this to be a puzzle solver, rather than a single word finder. For each word found, we could then recursively start a new exploration with those positions already visited. This might be computationally expensive. How can we optimize this? Maybe by finding candidate spangrams first, and then filling in the rest of the board.
+
+This is a task for another day. 🙂
---
+++ find-strands/strands.js
@@ -0,0 +1,96 @@
+const fs = require("fs");
+
+let wordList = fs
+  .readFileSync("words_alpha.txt", "utf8")
+  .split("\n")
+  .map((word) => word.trim())
+  .sort();
+
+// wordList = fs
+//   // https://www.spreadthewordlist.com/wordlist
+//   .readFileSync("spreadthewordlist.txt", "utf8")
+//   .split("\n")
+//   .map((line) => line.trim().split(";"))
+//   .filter(([a, b]) => Number(b) == 50)
+//   .map((a) => a[0])
+//   .sort();
+
+// binary search wordList for a word
+function searchWord(word) {
+  let low = 0;
+  let high = wordList.length - 1;
+  let mid;
+  while (low <= high) {
+    mid = Math.floor((low + high) / 2);
+    if (wordList[mid] === word) {
+      return [true, mid];
+    }
+    if (word < wordList[mid]) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return [false, low]; // return the location where it would be
+}
+
+function explore(row, col, word = "", visited = []) {
+  // exclude out of bounds
+  if (row < 0 || row >= puzzle.length || col < 0 || col >= puzzle[0].length) {
+    return;
+  }
+  const current = row * 10 + col;
+  // exclude visited
+  if (visited.includes(current)) {
+    return;
+  }
+
+  word += puzzle[row][col];
+  visited = [...visited, current];
+  if (word.length > 1) {
+    // binary search
+    let [found, index] = searchWord(word);
+    // no potential words
+    if (!wordList[index].startsWith(word)) {
+      return;
+    }
+    // print valid words
+    if (word.length > 3 && found) console.log(word);
+  }
+  // continue exploring
+  for (let i = -1; i <= 1; i++) {
+    for (let j = -1; j <= 1; j++) {
+      if (i === 0 && j === 0) {
+        continue;
+      }
+      explore(row + i, col + j, word, visited);
+    }
+  }
+}
+
+// march 31
+puzzle = `refnan
+ifloig
+buytal
+lnbnel
+oeiran
+nmuhft
+sllsie
+tarinr`
+  .split("\n")
+  .map((row) => row.split(""));
+
+//explore(0, 0);
+//explore(0, puzzle[0].length - 1);
+explore(puzzle.length - 1, 3);
+
+// puzzle = `ersiti
+// tebdfr
+// chaore
+// uuwapc
+// refnli
+// bfaich
+// ruuoai
+// ealnos`
+//   .split("\n")
+//   .map((row) => row.split(""));
